b840b76ccbbae074eb4cb51b9ee9896d08f71498
Alnoor Pirani <alnoorp@gmail.com>
2024-03-08 21:07:45 +0000

    Add spaceConfig.ts with theme metadata

---
+++ typescript/spaceConfig.ts
@@ -0,0 +1,77 @@
+export const spaceMetadata = {
+    theme: {
+      root: {
+        primaryColor: "#ff7a59",
+        dangerColor: "#F00028",
+        warningColor: "#FF9800",
+        actionColor: "#ff7a59",
+        buttonBorderRadius: "30px",
+        borderColor: "#ff7a59",
+      },
+      sidebar: {
+        logo: "https://asset.brandfetch.io/idRt0LuzRf/id5v3R8IHT.svg",
+        textColor: "#ffffff",
+        titleColor: "#ffffff",
+        focusBgColor: "#ff7a59",
+        focusTextColor: "#ffffff",
+        backgroundColor: "#2d3e50",
+        footerTextColor: "#ffffff",
+        textUltralightColor: "#fff1ee",
+      },
+      table: {
+        inputs: {
+          radio: { color: "#ff7a59" },
+          checkbox: { color: "#ff7a59" },
+        },
+        filters: {
+          color: "#000",
+          backgroundColor: "#f7f6f4",
+          active: {
+            color: "#fff",
+            backgroundColor: "#ff7a59",
+          },
+          error: {
+            activeBackgroundColor: "#F00028",
+          },
+        },
+        column: {
+          header: {
+            fontSize: "14px",
+            backgroundColor: "#f7f6f4",
+            borderColor: "#E5E4E2",
+            color: "#050d3a",
+            dragHandle: {
+              idle: "#ff7a59",
+              dragging: "#ff7a59",
+            },
+          },
+        },
+        fontFamily: "'Helvetica', sans-serif",
+        indexColumn: {
+          backgroundColor: "#f7f6f4",
+          borderColor: "#E5E4E2",
+          selected: {
+            color: "#000",
+            backgroundColor: "#EAF1FF",
+          },
+        },
+        cell: {
+          borderColor: "#E5E4E2",
+          selected: {
+            backgroundColor: "#EAF1FF",
+          },
+          active: {
+            borderColor: "#ff7a59",
+            spinnerColor: "#ff7a59",
+          },
+        },
+        boolean: {
+          toggleChecked: "#ff7a59",
+        },
+        loading: {
+          color: "#ff7a59",
+        },
+      },
+    },
+  };
+  
